82bac6f739261195a95e151170695ec821d410db
Tim Campbell <timetinytim@gmail.com>
2018-09-03 13:43:50 -0400

    Added docstrings

--- webhook-responder.py
+++ webhook-responder.py
@@ -1,14 +1,30 @@
 #!/usr/bin/python3
 
+"""
+Simple GitLab webhook listener template.
+
+GitLab allows the user to create webhooks to trigger external jobs whenever
+certain operations are invoked. This is a very basic template for being able
+to handle all possible webhook requests from GitLab.
+
+Parameters
+----------
+verbose : int
+    How verbose we want to be
+port : int
+    The port to listen on for requests
+"""
+
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import json
 import argparse
 import logging
 
-def some_function():
-    print("some_function got called")
-
 def create_args():
+    """
+    Sets up the arguments we need to listen for
+    """
+
     parser = argparse.ArgumentParser()
 
     parser.add_argument('-v', '--verbose', default=0,
@@ -18,31 +34,71 @@ def create_args():
     return parser
 
 def process_push():
+    """
+    Process a GitLab "push" operation.
+    """
     logging.warn('push functionality not implemented')
+    ### Your own code goes here ###
 
 def process_tag_push():
+    """
+    Process a GitLab "push" operation.
+    """
     logging.warn('tag_push functionality not implemented')
+    ### Your own code goes here ###
 
 def process_issue():
+    """
+    Process a GitLab "push" operation.
+    """
     logging.warn('issue functionality not implemented')
+    ### Your own code goes here ###
 
 def process_note():
+    """
+    Process a GitLab "push" operation.
+    """
     logging.warn('note functionality not implemented')
+    ### Your own code goes here ###
 
 def process_merge_request():
+    """
+    Process a GitLab "push" operation.
+    """
     logging.warn('merge_request functionality not implemented')
+    ### Your own code goes here ###
 
 def process_wiki_page():
+    """
+    Process a GitLab "push" operation.
+    """
     logging.warn('wiki_page functionality not implemented')
+    ### Your own code goes here ###
 
 def process_pipeline():
+    """
+    Process a GitLab "push" operation.
+    """
     logging.warn('pipeline functionality not implemented')
+    ### Your own code goes here ###
 
 def process_build():
-    logging.warn('build functionality not implemented')
+    """
+    Process a GitLab "push" operation.
+    """
+    logging.warn('build functionality not implemented')\
+    ### Your own code goes here ###
 
 class GitlabHandler(BaseHTTPRequestHandler):
+    """
+    Simple HTTP request handler for responding to GitLab webhooks.
+    """
+
     def _set_response(self):
+        """
+        Sends back an HTTP 200 response
+        """
+
         message = b'OK'
         self.send_response(200)
         self.send_header('Content-type', 'text')
@@ -51,6 +107,10 @@ class GitlabHandler(BaseHTTPRequestHandler):
         self.wfile.write(message)
 
     def _process_request(self, request_kind):
+        """
+        Figures out what kind of request we have, and which function to call
+        """
+
         # Request type -> function links
         switches = {
             'push':          process_push,
@@ -68,6 +128,9 @@ class GitlabHandler(BaseHTTPRequestHandler):
         func()
 
     def do_POST(self):
+        """
+        Respond to a POST request (the only request GitLab should be sending)
+        """
 
         # Extract the JSON
         data_string = self.rfile.read(int(self.headers['Content-Length']))
@@ -82,6 +145,9 @@ class GitlabHandler(BaseHTTPRequestHandler):
         # Send off to the proper function
         self._process_request(request['object_kind'])
 
+#######################################
+#            Main Function            #
+#######################################
 if (__name__ == '__main__'):
     # Get args
     parser = create_args()
